249ef7f5397bd4f3d524459f470b1d82e347fe06
木槿科技 <sunyuandong@mujinkeji.com>
2018-06-25 16:29:38 +0800

    Initial Commit

---
+++ RAC_MVVM/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RAC_MVVM
+//
+//  Created by 木槿科技 on 2018/6/25.
+//  Copyright © 2018年 深圳市木槿科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ RAC_MVVMTests/RAC_MVVMTests.m
@@ -0,0 +1,39 @@
+//
+//  RAC_MVVMTests.m
+//  RAC_MVVMTests
+//
+//  Created by 木槿科技 on 2018/6/25.
+//  Copyright © 2018年 深圳市木槿科技有限公司. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RAC_MVVMTests : XCTestCase
+
+@end
+
+@implementation RAC_MVVMTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
